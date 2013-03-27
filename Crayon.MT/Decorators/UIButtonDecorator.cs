7ba62617361ad7611eda7ee53b8ab35f7cc94ec6
jamilgeor <jamilgeor@gmail.com>
2013-03-27 21:03:30 +1200

    Added font size and family support to buttons.

--- Crayon.MT/Decorators/UIButtonDecorator.cs
+++ Crayon.MT/Decorators/UIButtonDecorator.cs
@@ -21,6 +21,18 @@ namespace Crayon.MT
 			var color = UIColor.FromRGBA (property.Color.R, property.Color.G, property.Color.B, property.Color.A);
 			_buttonView.SetTitleColor (color, UIControlState.Normal);
 		}
+
+		[StyleProperty(typeof(StyleFontFamilyProperty))]
+		public void SetFontFace(StyleFontFamilyProperty property)
+		{
+			_buttonView.Font = UIFont.FromName (property.FontName, _buttonView.Font.PointSize);
+		}
+
+		[StyleProperty(typeof(StyleFontSizeProperty))]
+		public void SetFontSize(StyleFontSizeProperty property)
+		{
+			_buttonView.Font = UIFont.FromName (_buttonView.Font.Name, property.Size);
+		}
 	}
 }
 

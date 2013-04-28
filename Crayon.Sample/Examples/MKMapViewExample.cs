89a201f25cf6f66bf9ac4c09a381702ba504d81e
jamilgeor <jamilgeor@gmail.com>
2013-04-28 21:40:43 +1300

    Added MKAnnotationView example.

--- Crayon.Sample/Examples/MKMapViewExample.cs
+++ Crayon.Sample/Examples/MKMapViewExample.cs
@@ -2,6 +2,8 @@ using System;
 using MonoTouch.UIKit;
 using MonoTouch.MapKit;
 using Crayon.MT;
+using MonoTouch.CoreLocation;
+using MonoTouch.Foundation;
 
 namespace Crayon.Sample
 {
@@ -19,6 +21,46 @@ namespace Crayon.Sample
 			View.SetStyleClass("sample-background");
 			
 			View.AddSubview(_mapView);
+
+			AddAnnotation();
+		}
+
+		void AddAnnotation()
+		{
+			_mapView.GetViewForAnnotation = GetViewForAnnotation;
+
+			var annotation = new ExampleAnnotation
+			{
+				Coordinate = new CLLocationCoordinate2D(37.81, -122.477989) 
+			};
+
+			annotation.SetTitle("Example Annotation");
+
+			_mapView.AddAnnotation(annotation);
+		}
+
+		MKAnnotationView GetViewForAnnotation (MKMapView mapView, NSObject annotation)
+		{
+			var annotationView = new MKAnnotationView(annotation, "ExampleAnnotation");
+
+			annotationView.CanShowCallout = true;
+
+			annotationView.SetStyleId("sample-annotation");
+
+			return annotationView;
+		}
+
+		class ExampleAnnotation : MKAnnotation
+		{
+			string _title;
+
+			public override string Title { get { return _title; } }
+			public override CLLocationCoordinate2D Coordinate { get; set; }
+
+			public void SetTitle(string title)
+			{
+				_title = title;
+			}
 		}
 	}
 }
